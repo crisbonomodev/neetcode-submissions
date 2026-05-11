ba0fb38c25fd05bf9a72e3122fd918b23c59633d
Cristian Bonomo <48805494+crisbonomodev@users.noreply.github.com>
2026-05-11 05:54:41 -0300

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length) {return false}
+
+        const counter = new Array(26).fill(0)
+
+        for (let i = 0; i < s.length; i++) {
+        counter[s.charCodeAt(i) - 'a'.charCodeAt(0)]++
+        counter[t.charCodeAt(i) - 'a'.charCodeAt(0)]--
+        }
+
+        for (const num of counter) {
+             if(num != 0) {return false}
+        }
+        return true
+    }
+}
