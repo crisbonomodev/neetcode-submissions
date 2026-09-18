2b67358197525aac2b2507974ab7847a2923f2e9
Cristian Bonomo <48805494+crisbonomodev@users.noreply.github.com>
2026-09-18 05:42:53 -0300

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        if (prices.length <= 1) {return 0}
+
+        let min = prices[0]
+        let profit = 0
+
+        for (let fast = 0; fast < prices.length; fast++) {
+            if (prices[fast] < min) {
+                min = prices[fast]
+            }
+            if (prices[fast] - min > profit) {
+                profit = prices[fast] - min
+            }
+        }
+
+        return profit
+    }
+}
